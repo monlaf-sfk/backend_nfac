4f1e737a20e4ceb63b6c985d58c30d291cf0be48
rasulkerimzhanov <naiosobzorigr@gmail.com>
2025-04-26 10:44:02 +0500

    .

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ from pydantic import AnyHttpUrl
 
 class Settings(BaseSettings):
     API_COINGECKO: str
-    API_COINGECKO_BASE_URL: AnyHttpUrl = "https://api.coingecko.com/api"
+    API_COINGECKO_BASE_URL: AnyHttpUrl = "https://api.coingecko.com/api/"
     FRONTEND_URL: AnyHttpUrl = "http://localhost:5173"
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
